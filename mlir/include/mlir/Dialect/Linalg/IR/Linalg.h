81284e54508bfccf83895f5972c39f90495f39cb
Karl Friebel <karl.friebel@friebelnet.de>
2022-06-29 15:57:04 +0200

    Apply suggestions from code review
    Co-authored-by: Liam Fitzpatrick <liam.fitzpatrick@xilinx.com>

--- mlir/include/mlir/Dialect/Linalg/IR/Linalg.h
+++ mlir/include/mlir/Dialect/Linalg/IR/Linalg.h
@@ -32,6 +32,10 @@
 namespace mlir {
 namespace linalg {
 
+/// Returns a singelton FallbackModel for the OperatorClassInterface.
+///
+/// This FallbackModel is attached to all linalg dialect operations so that we
+/// don't have to change their definitions.
 void* getOperatorClassInterfaceFallback();
 
 class LinalgOp;
