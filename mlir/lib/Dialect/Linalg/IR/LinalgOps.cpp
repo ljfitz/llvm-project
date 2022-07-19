cd31b3c06f5982c105a5fe61734c018d4a0296af
Dominik Montada <dominik.montada@xilinx.com>
2022-07-19 10:23:23 +0100

    feat(LinalgOps): FXML-843 classify FillOp as broadcast

--- mlir/lib/Dialect/Linalg/IR/LinalgOps.cpp
+++ mlir/lib/Dialect/Linalg/IR/LinalgOps.cpp
@@ -2260,12 +2260,12 @@ struct OperatorClassInterfaceFallback
 
     if (mlir::detail::isConstantLike(op) || isa<InitTensorOp>(op))
       return OperatorClass::Constant;
-    if (isa<ApplyBias2DFchwOp, tensor::SplatOp, linalg::BroadcastBias2DFchwOp>(
-            op))
+    if (isa<ApplyBias2DFchwOp, tensor::SplatOp, linalg::BroadcastBias2DFchwOp,
+            linalg::FillOp>(op))
       return OperatorClass::Broadcast;
     if (isa<Relu2DNchwOp, Lrelu2DNchwOp>(op))
       return OperatorClass::Activation;
-    if (isa<tensor::PadOp, linalg::FillOp>(op))
+    if (isa<tensor::PadOp>(op))
       return OperatorClass::Padding;
     if (isa<Conv2DReluOp, Conv2DLreluOp>(op))
       return OperatorClass::Convolution | OperatorClass::Activation;
